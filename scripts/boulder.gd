9b5fa12d95fca1feda1812131ef62ff887c2ec65
Ian Day <ian.day21@gmail.com>
2024-04-30 14:38:02 -0400

    Upload #10
    - BOULDER
    - MIsc.

---
+++ scripts/boulder.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") #Gravity from Project Settings (9.81)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
