a04c6e4fff715f1d3852fd50c881b7332eefde4e
Ian Day <109267942+Wet-Hands@users.noreply.github.com>
2024-06-06 14:57:41 -0400

    Menu Fix and Goblin Fix (FOR REAL THIS TIME)

--- scripts/goblin.gd
+++ scripts/goblin.gd
@@ -1,80 +1,76 @@
 extends CharacterBody3D
 
-@export var shaderT = 4.5
-var maxHealth = 50
-var curHealth = maxHealth
-var speed = 1.5
+@export var shaderT = 4.5 #Length of the Death Shader (Seconds)
+var maxHealth = 60 #Maximum Goblin Health
+var curHealth = maxHealth #Current Goblin Health
+var speed = 1.5 #Goblin Speed
 
-@onready var navAgent = $NavigationAgent3D
-@onready var player
-@onready var shaderMat = preload("res://shaders/dissolveGoblin.tres")
+@onready var navAgent = $NavigationAgent3D #Navigation for Goblin
+@onready var player #Player Reference
+@onready var shaderMat = preload("res://shaders/dissolveGoblin.tres") #Shader Material
 
-var isDead = false
-var isAtk = false
+var isDead = false #Is Goblin Dead? (No)
+var isAtk = false #Is Goblin Attacking? (No)
+var active = false #Is Goblin Active? (No)
 
-var active = false
 func _ready():
-	$Skeleton3D/Goblin.material_override.set_shader_parameter("LightStrength", 0) #Update Shader on Skeleton
-	shaderT = 4.5
-	active = false
+	$Skeleton3D/Goblin.material_override.set_shader_parameter("LightStrength", 0) #Disable Shader Light when Spawned
+	shaderT = 4.5 #Set Shader Time
+	active = false #Disable Goblin
 
-func _process(delta):
-	velocity = Vector3.ZERO
-	navAgent.set_target_position(player.global_transform.origin)
-	var nextNavPoint = navAgent.get_next_path_position()
-	velocity = (nextNavPoint - global_transform.origin).normalized() * speed
-	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
-	if active == true:
-		move_and_slide()
-	if isAtk == false && isDead == false:
-		$AnimationPlayer.speed_scale = 1
-		$AnimationPlayer.play("walk")
+func _process(delta): #While Goblin Scene is in Game
+	velocity = Vector3.ZERO #Initial Velocity at Zero
+	navAgent.set_target_position(player.global_transform.origin) #Set Target Position
+	var nextNavPoint = navAgent.get_next_path_position() #Next Target Position
+	velocity = (nextNavPoint - global_transform.origin).normalized() * speed #Move to Target
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP) #Look at Player
+	if active == true: #If Goblin is Active
+		move_and_slide() #Move Goblin
+	if isAtk == false && isDead == false: #If Goblin is not Attacking or Dead
+		$AnimationPlayer.speed_scale = 1 #Animation Speed at 1.0 (Base)
+		$AnimationPlayer.play("walk") #Play Walk Animation
 
-func health(num):
-	curHealth += num
-	if curHealth <= 0:
-		isDead = true
-		$Skeleton3D/Goblin.material_override.set_shader_parameter("LightStrength", 1) #Update Shader on Skeleton
-		$ShaderTimer.start()
-		$AnimationPlayer.play("Death")
-		$ShaderAnim.play("shaderOn")
-		$Gdeath.play()
+func health(num): #Goblin Health is Changed
+	curHealth += num #Add Health to Current Health (Usually 'num' is negative)
+	if curHealth <= 0: #If Current Health is at Zero
+		isDead = true #Goblin Is Dead
+		$Skeleton3D/Goblin.material_override.set_shader_parameter("LightStrength", 1) #Turn on Shader Health
+		$ShaderTimer.start() #Start Shader Timer
+		$AnimationPlayer.play("Death") #Play Death Animation
+		$ShaderAnim.play("shaderOn") #Start Shader Animation
+		$Gdeath.play() #Play Death Sound Effect
+	else: #If Goblin Survives Hit
+		$Ghurt.play() #Play Hurt Sound Effect
 
-func _on_area_3d_area_entered(area):
-	if area.is_in_group("sword"):
-		$Ghurt.play()
-		health(-25)
+func _on_area_3d_area_entered(area): #If Goblin Hitbox Entered
+	if area.is_in_group("sword"): #If Goblin is hit by Player's Sword
+		health(-25) #Lose 25 Health
 
-func _on_shader_timer_timeout():
-	$ShaderTimer.stop()
-	$AtkArea.remove_from_group("goblinAtk")
-	if shaderT > 1.0:
-		$Skeleton3D/Goblin.material_override.set_shader_parameter("ShaderTime", shaderT) #Update Shader on Skeleton
-		$ShaderTimer.start()
-	if shaderT == 1.0:
-		queue_free()
+func _on_shader_timer_timeout(): #When Shader Timer runs out (0.1s)
+	$ShaderTimer.stop() #Stop Shader Timer
+	$AtkArea.remove_from_group("goblinAtk") #Disable Goblin Attack Range
+	if shaderT > 1.0: #If ShaderTime is above 1s
+		$Skeleton3D/Goblin.material_override.set_shader_parameter("ShaderTime", shaderT) #Update Shader Amount on Goblin
+		$ShaderTimer.start() #Start Shader Timer
+	if shaderT == 1.0: #If Shader Time is at 1.0s
+		queue_free() #Send Goblin back to Hell
 
-func _on_int_area_area_entered(area):
-	if area.is_in_group("player"):
-		active = true
+func _on_int_area_area_entered(area): #If Goblin Activiation Range is Entered
+	if area.is_in_group("player"): #If Player enters Activiation Range
+		active = true #Goblin is Active
 
-func _on_atk_area_area_entered(area):
-	if area.is_in_group("player"):
-		if isDead == false:
-			$AnimationPlayer.play("attack")
-			isAtk = true
+func _on_atk_area_area_entered(area): #If Goblin Attack Range is Entered
+	if area.is_in_group("player"): #If Goblin Attack Range is Entered by Player 
+		if isDead == false: #If Goblin is Alive
+			$AnimationPlayer.play("attack") #Play Attack Animation
+			isAtk = true #Goblin Is Attacking
 
-func _on_atk_area_area_exited(area):
-	isAtk = false
+func _on_atk_area_area_exited(area): #If Goblin Attack Range is Exited
+	isAtk = false #Goblin Isn't Attacking
 
-func attack():
-	$AtkArea/CollisionShape3D.disabled = true
-	isAtk = true
+func attackEnd(): #When Goblin Attack Animation Ends
+	$AtkArea/CollisionShape3D.disabled = true #Disable Goblin Attack Collision
+	if isAtk == true && isDead == false: #If Player is in Attack Range and Goblin is Alive
+		$AnimationPlayer.play("attack") #Play Goblin Attack Animation
+		$AtkArea/CollisionShape3D.disabled = false #Enable Goblin Attack Collision
 
-func attackEnd():
-	$AtkArea/CollisionShape3D.disabled = false
-
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "attack":
-		if isAtk == true && isDead == false:
-			attack()
