92e8030f973b5954b4cd0b1fd29c8691e1a5ba3f
Ian Day <ian.day21@gmail.com>
2024-09-26 13:49:13 -0400

    Character Added

--- scenes/UI/level_menu.gd
+++ scenes/UI/level_menu.gd
@@ -1,8 +1,15 @@
 extends Control
 
+var slide1 : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://levels/level1/level_1.tscn")
+	if slide1 == false:
+		slide1 = true
+		$Text1.visible = false
+		$Text2.visible = true
+	elif slide1 == true:
+		get_tree().change_scene_to_file("res://levels/level1/level_1.tscn")
